fbf4855a0901a833b8e1233bd567a8cceded1067
spectre <e.sahinnn61@gmail.com>
2024-04-19 16:41:18 -0700

    method post

--- Backend/app.js
+++ Backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const bookRoutes = require("./routes/book");
 const userRoutes = require("./routes/user");
@@ -32,5 +33,8 @@ app.use(bodyParser.json());
 
 app.use("/api/books", bookRoutes);
 app.use("/api/auth", userRoutes);
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 module.exports = app;
+
+// export PATH="/Users/Elyesa1/.npm-global/bin:$PATH"
--- Backend/routes/book.js
+++ Backend/routes/book.js
@@ -1,12 +1,14 @@
 const express = require("express");
-const auth = require("../middleware/auth");
 const router = express.Router();
+
+const auth = require("../middleware/auth");
+const multer = require("../middleware/multer-config");
 
 const bookCtrl = require("../controllers/book");
 
-router.post("/", auth, bookCtrl.createBook);
+router.post("/", auth, multer, bookCtrl.createBook);
 
-router.put("/:id", auth, bookCtrl.modifyBook);
+router.put("/:id", auth, multer, bookCtrl.modifyBook);
 
 router.delete("/:id", auth, bookCtrl.deleteBook);
 
