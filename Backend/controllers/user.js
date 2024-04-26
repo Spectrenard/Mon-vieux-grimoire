6df9930e75ef7187e307d7fe176726098b3495bd
spectre <e.sahinnn61@gmail.com>
2024-04-26 16:42:19 +0200

    ajout regex email

--- Backend/controllers/user.js
+++ Backend/controllers/user.js
@@ -3,6 +3,11 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
 exports.signup = (req, res, next) => {
+  // Vérifier si l'e-mail est valide
+  if (!/\S+@\S+\.\S+/.test(req.body.email)) {
+    return res.status(400).json({ error: "L'e-mail est invalide !" });
+  }
+
   bcrypt
     .hash(req.body.password, 10)
     .then((hash) => {
@@ -12,7 +17,7 @@ exports.signup = (req, res, next) => {
       });
       user
         .save()
-        .then(() => res.status(201).json({ message: "Utilisateur crée !" }))
+        .then(() => res.status(201).json({ message: "Utilisateur créé !" }))
         .catch((error) => res.status(400).json({ error }));
     })
     .catch((error) => res.status(500).json({ error }));
